ee160af92d884003936f97055cdcebd0dd815efd
tuyur <tuyur4444@gmail.com>
2020-03-19 21:41:50 +0300

    compare

--- Program.cs
+++ Program.cs
@@ -2,16 +2,20 @@
 
 namespace Delegate
 {
-    public delegate Word[] Counter(Word[] words, int x);
+    public delegate bool Counter(int x, int y);
 
     public class Word
     {
         public int index { get; set; }
         public string word { get; set; }
 
-        public static Word[] counter(Word[] words, int i, Counter counter)
+        public static string compare(int x, int y, Counter counter)
         {
-            return counter(words, i);
+            if (counter(x, y))
+                return String.Format(" {0} , {1} den büyüktür", x, y);
+            else
+                return String.Format(" {0} , {1} den küçük veya eşittir", x, y);
+
         }
 
     }
@@ -20,36 +24,10 @@ namespace Delegate
     {
         static void Main(string[] args)
         {
-            Word[] words = new Word[]{
-                new Word { index = 0 , word = "proclaiming"},
-                new Word { index = 1 , word = "brilliant"},
-                new Word { index = 2 , word = "exhibition"},
-                new Word { index = 3 , word = "ingenious"},
-                new Word { index = 4 , word = "presents"},
-                new Word { index = 4 , word = "car"}
-            };
 
-            var counterThanSmaller = Word.counter(words, 4, (Word[] words, int x) =>
-           {
-               var w = new Word[words.Length];
-               int i = -1;
-               foreach (var word in words)
-               {
-                   if (word.word.Length > x)
-                   {
-                       i++;
-                       w[i] = word;
-                   }
-               }
-               return w;
-           });
+            var compareString = Word.compare(3, 4, (x, y) => x == y);
+            System.Console.WriteLine(compareString);
 
-            System.Console.WriteLine("Smaller Than 4");
-            foreach (Word s in counterThanSmaller)
-            {
-                if (s != null)
-                    Console.WriteLine(s.word);
-            }
 
 
         }
